aa13179c24d9b7bb413810dedc8d757ed2df03d4
pp <pp@example.com>
2025-05-03 20:34:53 +0200

    add cookz plants logo

--- ModsMetaData/@CookZPlants/mod.cpp
+++ ModsMetaData/@CookZPlants/mod.cpp
@@ -1,7 +1,7 @@
 name = "CookZPlants";
-picture = "CookZPlants\data\cookZLogo.edds"; // picture in expanded description
-logoSmall = "CookZPlants\data\cookZLogo.edds"; // icon next to mod name when description is not expanded
-logo = "CookZPlants\data\cookZLogo.edds"; // logo below game menu
+picture = "CookZPlants\data\cookZPlantsLogo.edds"; // picture in expanded description
+logoSmall = "CookZPlants\data\cookZPlantsLogo.edds"; // icon next to mod name when description is not expanded
+logo = "CookZPlants\data\cookZPlantsLogo.edds"; // logo below game menu
 logoOver = "Cook the mouse over!"; // on mouse hover over logo
 tooltip = "Cook it!"; // tool tip on mouse hover
 overview = "Provides some plants and recipes for pan and pot."; // overview
