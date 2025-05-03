5c2ce17657168cec410c75bbfddfd5f64446688c
pp <pp@example.com>
2025-05-03 16:24:14 +0200

    register more "all" ingredients

--- CookZPlants/Scripts/4_World/modded/registeranyingredients.c
+++ CookZPlants/Scripts/4_World/modded/registeranyingredients.c
@@ -6,5 +6,8 @@ modded class CookZ_Cookbook
 
         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Chili");
         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Corn");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Onion");
+
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_SAUSAGE, "CookZPlants_Tofu_Sausage");
     }
 }
