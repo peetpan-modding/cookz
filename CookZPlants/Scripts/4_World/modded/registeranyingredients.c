931b7e3aed3623a553605d5a37893e5a3d954f07
pp <pp@example.com>
2025-05-03 20:01:17 +0200

    disable any registration until cookz releases the override mechanics

--- CookZPlants/Scripts/4_World/modded/registeranyingredients.c
+++ CookZPlants/Scripts/4_World/modded/registeranyingredients.c
@@ -1,13 +1,14 @@
-modded class CookZ_Cookbook
-{
-    override void RegisterAnyIngredients()
-    {
-        super.RegisterAnyIngredients();
+// ToDo pp - disabled for now until CookZ allows the override
+// modded class CookZ_Cookbook
+// {
+//     override void RegisterAnyIngredients()
+//     {
+//         super.RegisterAnyIngredients();
 
-        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Chili");
-        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Corn");
-        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Onion");
+//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Chili");
+//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Corn");
+//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Onion");
 
-        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_SAUSAGE, "CookZPlants_Tofu_Sausage");
-    }
-}
+//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_SAUSAGE, "CookZPlants_Tofu_Sausage");
+//     }
+// }
