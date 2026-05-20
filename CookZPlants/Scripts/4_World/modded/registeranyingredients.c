9726eb95f7228314c6c11ead56adde97005c64d1
pp <pp@example.com>
2026-05-20 13:18:31 +0200

    CookZPlants: enable register any ingredients

--- CookZPlants/Scripts/4_World/modded/registeranyingredients.c
+++ CookZPlants/Scripts/4_World/modded/registeranyingredients.c
@@ -1,14 +1,16 @@
-// ToDo pp - disabled for now until CookZ allows the override
-// modded class CookZ_Cookbook
-// {
-//     override void RegisterAnyIngredients()
-//     {
-//         super.RegisterAnyIngredients();
+modded class CookZ_Cookbook
+{
+    override void RegisterAnyIngredients()
+    {
+        super.RegisterAnyIngredients();
 
-//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Chili");
-//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Corn");
-//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Onion");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Chili");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Corn");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Onion");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_SugarBeet");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_CabbageSlice");
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_VEG, "CookZPlants_Cucumber");
 
-//         RegisterAnyIngredient(COOKING_INGREDIENT_ANY_SAUSAGE, "CookZPlants_Tofu_Sausage");
-//     }
-// }
+        RegisterAnyIngredient(COOKING_INGREDIENT_ANY_SAUSAGE, "CookZPlants_Tofu_Sausage");
+    }
+}
